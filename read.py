92153a3a65f6bcbe4ad224d41987698281dc3ba9
azbndr <azbndra@yahoo.com>
2019-03-29 19:19:53 -0500

    Using UDP p2p aproach test

--- read.py
+++
@@ -1,5 +0,0 @@
-from urllib import urlopen
-
-while True:
-	data = urlopen('http://192.168.1.5/server/test').read()
-
---
+++ upd.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python3
+
+import socket, threading
+
+IP = '127.0.0.1'
+BUFFER_SIZE = 1024
+
+snd = rec = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+rec.bind((IP, 5005))
+
+def listen():
+    while True:
+        data, addr = rec.recvfrom(BUFFER_SIZE)
+        print("received message:", data.decode())
+
+threading.Thread(target=listen).start()
+
+### OUR PROGRAM
+while True:
+    snd.sendto(str.encode(input()), (IP, 5006))
