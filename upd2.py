c81fee489a3c1ba2e645620fe7d881a3225f6bfe
azbndr <azbndra@yahoo.com>
2019-03-30 07:25:13 -0500

    Transeiver Test 1

--- upd2.py
+++ upd2.py
@@ -2,19 +2,24 @@
 
 import socket, threading
 
+x = []
 IP = '127.0.0.1'
 BUFFER_SIZE = 1024
 
 snd = rec = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
 rec.bind((IP, 5006))
 
-def listen():
+def listen(data):
     while True:
-        data, addr = rec.recvfrom(BUFFER_SIZE)
-        print("received message:", data.decode())
+        x = rec.recvfrom(BUFFER_SIZE)[0].decode()
+        data[0] = x
 
-threading.Thread(target=listen).start()
+threading.Thread(target=listen, args=x).start()
 
 ### OUR PROGRAM
-while True:
-    snd.sendto(str.encode(input()), (IP, 5005))
+def send(data):
+    snd.sendto(str.encode(str(data[0])), (IP, 5005))
+    print(data[0])
+
+x[0] = int(x[0]) + 1
+send(x)
